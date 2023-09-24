9d67615f9adba27dac17da5f6312b5c8f49910e4
Cyrus245 <you@example.com>
2023-09-24 20:43:44 +0600

    -ui built

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def get_home():  # put application's code here
+    blog_data = requests.get('https://api.npoint.io/65af5396f563b009b3dd').json()
+    return render_template('index.html', data=blog_data)
+
+
+@app.route('/about')
+def get_about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def get_contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
